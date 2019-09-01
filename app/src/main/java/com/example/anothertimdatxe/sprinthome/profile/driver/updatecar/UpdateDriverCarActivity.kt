f536246413f541a6def8be4dd88681d69c648aa0
HoangVietSE <viethoangtien@gmail.com>
2019-09-01 23:07:51 +0700

    matisse library image picker android

--- app/src/main/java/com/example/anothertimdatxe/sprinthome/profile/driver/updatecar/UpdateDriverCarActivity.kt
+++ app/src/main/java/com/example/anothertimdatxe/sprinthome/profile/driver/updatecar/UpdateDriverCarActivity.kt
@@ -2,6 +2,7 @@ package com.example.anothertimdatxe.sprinthome.profile.driver.updatecar
 
 import android.app.Activity
 import android.content.Intent
+import android.content.pm.ActivityInfo
 import android.content.pm.PackageManager
 import android.os.Build
 import android.provider.MediaStore
@@ -24,8 +25,14 @@ import com.example.anothertimdatxe.extension.visible
 import com.example.anothertimdatxe.util.DateUtil
 import com.example.anothertimdatxe.util.ToastUtil
 import com.example.anothertimdatxe.widget.DatePickerDialogWidget
+import com.example.anothertimdatxe.widget.GifSizeFilter
+import com.example.anothertimdatxe.widget.Glide4Engine
+import com.zhihu.matisse.Matisse
+import com.zhihu.matisse.MimeType
+import com.zhihu.matisse.filter.Filter
 import kotlinx.android.synthetic.main.activity_update_driver_car.*
 import kotlinx.android.synthetic.main.item_add.view.*
+
 
 class UpdateDriverCarActivity : BaseActivity<UpdateDriverCarPresenter>(), UpdateDriverCarView {
     private var mImageAdapter: ImageAdapter? = null
@@ -39,7 +46,7 @@ class UpdateDriverCarActivity : BaseActivity<UpdateDriverCarPresenter>(), Update
     private var mCarID: Int? = null
     private var isSpinnerCarName: Boolean = true
     override val layoutRes: Int
-        get() = R.layout.activity_update_driver_car
+        get() = com.example.anothertimdatxe.R.layout.activity_update_driver_car
 
     companion object {
         const val REQUEST_PERMISSION_ALBUM = 1000
@@ -127,16 +134,16 @@ class UpdateDriverCarActivity : BaseActivity<UpdateDriverCarPresenter>(), Update
     private fun setUpToolbar() {
         toolbar?.let {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                it.setBackgroundColor(resources.getColor(R.color.colorPrimary, null))
+                it.setBackgroundColor(resources.getColor(com.example.anothertimdatxe.R.color.colorPrimary, null))
             } else {
-                it.setBackgroundColor(resources.getColor(R.color.colorPrimary))
+                it.setBackgroundColor(resources.getColor(com.example.anothertimdatxe.R.color.colorPrimary))
             }
         }
         leftbutton?.setOnClickListener {
             finish()
         }
         toolbarTitle?.let {
-            it.text = resources.getString(R.string.update_driver_car_title).toUpperCase()
+            it.text = resources.getString(com.example.anothertimdatxe.R.string.update_driver_car_title).toUpperCase()
 
         }
     }
@@ -169,7 +176,7 @@ class UpdateDriverCarActivity : BaseActivity<UpdateDriverCarPresenter>(), Update
         when (requestCode) {
             REQUEST_PERMISSION_ALBUM -> {
                 if (grantResults != null && grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_DENIED) {
-                    ToastUtil.show(resources.getString(R.string.request_read_external_storeage))
+                    ToastUtil.show(resources.getString(com.example.anothertimdatxe.R.string.request_read_external_storeage))
                 } else {
                     selectImageFromAlbum()
                 }
@@ -178,11 +185,22 @@ class UpdateDriverCarActivity : BaseActivity<UpdateDriverCarPresenter>(), Update
     }
 
     private fun selectImageFromAlbum() {
-        val intent = Intent()
-        intent.setType("image/*")
-        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
-        intent.setAction(Intent.ACTION_GET_CONTENT)
-        startActivityForResult(Intent.createChooser(intent, "Select Image"), SELECT_IMAGE_FROM_ALBUM)
+//        val intent = Intent()
+//        intent.setType("image/*")
+//        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
+//        intent.setAction(Intent.ACTION_GET_CONTENT)
+//        startActivityForResult(Intent.createChooser(intent, "Select Image"), SELECT_IMAGE_FROM_ALBUM)
+        Matisse.from(this@UpdateDriverCarActivity)
+                .choose(MimeType.ofAll())
+                .countable(true)
+                .maxSelectable(5)
+                .addFilter(GifSizeFilter(320, 320, 5 * Filter.K * Filter.K))
+                .gridExpectedSize(resources.getDimensionPixelSize(com.example.anothertimdatxe.R.dimen.grid_expected_size))
+                .restrictOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED)
+                .thumbnailScale(0.85f)
+                .imageEngine(Glide4Engine())
+                .theme(R.style.Matisse_Zhihu)
+                .forResult(SELECT_IMAGE_FROM_ALBUM)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -190,30 +208,41 @@ class UpdateDriverCarActivity : BaseActivity<UpdateDriverCarPresenter>(), Update
             SELECT_IMAGE_FROM_ALBUM -> {
                 if (resultCode == Activity.RESULT_OK && data != null) {
                     val filePathColumn = arrayOf(MediaStore.Images.Media.DATA)
-                    if (data.data != null) {//select one image
-                        val uriImage = data.data
-                        val cursor = contentResolver.query(uriImage, filePathColumn, null, null, null, null)
+                    val mSelected = Matisse.obtainResult(data)
+                    for (i in 0..mSelected.size - 1) {
+                        val cursor = contentResolver.query(mSelected.get(i), filePathColumn, null, null, null, null)
                         cursor.moveToFirst()
                         val columnIndex = cursor.getColumnIndex(filePathColumn[0])
                         val absoluteFilePath = cursor.getString(columnIndex)
-                        val mDriverCarImage = DriverCarImage(absoluteFilePath, uriImage, ImageAdapter.VIEW_TYPE_IMAGE, true)
-                        mPresenter?.addImage(mDriverCarImage)
-                    } else {
-                        if (data.clipData != null) {//select multiple image
-                            val mClipData = data.clipData
-                            for (i in 0..mClipData.itemCount - 1) {
-                                val uriImage = mClipData.getItemAt(i).uri
-                                val cursor = contentResolver.query(uriImage, filePathColumn, null, null, null, null)
-                                cursor.moveToFirst()
-                                val columnIndex = cursor.getColumnIndex(filePathColumn[0])
-                                val absoluteFilePath = cursor.getString(columnIndex)
-                                val mDriverCarImage = DriverCarImage(absoluteFilePath, uriImage, ImageAdapter.VIEW_TYPE_IMAGE, true)
-                                if (!isFull) {
-                                    mPresenter?.addImage(mDriverCarImage)
-                                }
-                            }
+                        val mDriverCarImage = DriverCarImage(absoluteFilePath, mSelected.get(i), ImageAdapter.VIEW_TYPE_IMAGE, true)
+                        if (!isFull) {
+                            mPresenter?.addImage(mDriverCarImage)
                         }
                     }
+//                    if (data.data != null) {//select one image
+//                        val uriImage = data.data
+//                        val cursor = contentResolver.query(uriImage, filePathColumn, null, null, null, null)
+//                        cursor.moveToFirst()
+//                        val columnIndex = cursor.getColumnIndex(filePathColumn[0])
+//                        val absoluteFilePath = cursor.getString(columnIndex)
+//                        val mDriverCarImage = DriverCarImage(absoluteFilePath, uriImage, ImageAdapter.VIEW_TYPE_IMAGE, true)
+//                        mPresenter?.addImage(mDriverCarImage)
+//                    } else {
+//                        if (data.clipData != null) {//select multiple image
+//                            val mClipData = data.clipData
+//                            for (i in 0..mClipData.itemCount - 1) {
+//                                val uriImage = mClipData.getItemAt(i).uri
+//                                val cursor = contentResolver.query(uriImage, filePathColumn, null, null, null, null)
+//                                cursor.moveToFirst()
+//                                val columnIndex = cursor.getColumnIndex(filePathColumn[0])
+//                                val absoluteFilePath = cursor.getString(columnIndex)
+//                                val mDriverCarImage = DriverCarImage(absoluteFilePath, uriImage, ImageAdapter.VIEW_TYPE_IMAGE, true)
+//                                if (!isFull) {
+//                                    mPresenter?.addImage(mDriverCarImage)
+//                                }
+//                            }
+//                        }
+//                    }
                 }
             }
         }
@@ -223,7 +252,7 @@ class UpdateDriverCarActivity : BaseActivity<UpdateDriverCarPresenter>(), Update
         val view = this.inflate(R.layout.item_add)
         val btnAdd: ImageView = view.iv_add
         val params: LinearLayout.LayoutParams = btnAdd.layoutParams as LinearLayout.LayoutParams
-        params.marginEnd = resources.getDimensionPixelSize(R.dimen.margin_8_dp)
+        params.marginEnd = resources.getDimensionPixelSize(com.example.anothertimdatxe.R.dimen.margin_8_dp)
         btnAdd.layoutParams = params
         btnAdd.setImageResource(R.drawable.bg_add_button_not_active)
         if (btnAdd.parent != null) {
@@ -312,54 +341,54 @@ class UpdateDriverCarActivity : BaseActivity<UpdateDriverCarPresenter>(), Update
     }
 
     override fun onCarBrandError() {
-        ToastUtil.show(resources.getString(R.string.update_driver_car_error_car_brand))
+        ToastUtil.show(resources.getString(com.example.anothertimdatxe.R.string.update_driver_car_error_car_brand))
     }
 
     override fun onCarNameSpinnerError() {
-        ToastUtil.show(resources.getString(R.string.update_driver_car_error_car_name_spinner))
+        ToastUtil.show(resources.getString(com.example.anothertimdatxe.R.string.update_driver_car_error_car_name_spinner))
     }
 
     override fun onCarNameEdittextError() {
-        edt_car_name.setError(resources.getString(R.string.update_driver_car_error_car_name_edittext))
+        edt_car_name.setError(resources.getString(com.example.anothertimdatxe.R.string.update_driver_car_error_car_name_edittext))
         edt_car_name.requestFocus()
     }
 
     override fun onDoixeError() {
-        ToastUtil.show(resources.getString(R.string.update_driver_car_error_doixe))
+        ToastUtil.show(resources.getString(com.example.anothertimdatxe.R.string.update_driver_car_error_doixe))
     }
 
     override fun onNumberSeatError() {
-        edt_number_seat.setError(resources.getString(R.string.update_driver_car_error_number_seat))
+        edt_number_seat.setError(resources.getString(com.example.anothertimdatxe.R.string.update_driver_car_error_number_seat))
         edt_number_seat.requestFocus()
     }
 
     override fun onLicensePlateError() {
-        edt_license_plate.setError(resources.getString(R.string.update_driver_car_error_license_plate))
+        edt_license_plate.setError(resources.getString(com.example.anothertimdatxe.R.string.update_driver_car_error_license_plate))
         edt_license_plate.requestFocus()
     }
 
     override fun onColorError() {
-        edt_color.setError(resources.getString(R.string.update_driver_car_error_color))
+        edt_color.setError(resources.getString(com.example.anothertimdatxe.R.string.update_driver_car_error_color))
         edt_color.requestFocus()
     }
 
     override fun onRegistrationNotLessThanRegiterDate() {
-        edt_handangkiem.setError(resources.getString(R.string.update_driver_car_error_registration_not_less_than_regiter_date))
+        edt_handangkiem.setError(resources.getString(com.example.anothertimdatxe.R.string.update_driver_car_error_registration_not_less_than_regiter_date))
         edt_handangkiem.requestFocus()
     }
 
     override fun onDateRegisEmptyError() {
-        edt_date_regis.setError(resources.getString(R.string.update_driver_car_error_date_regis_empty))
+        edt_date_regis.setError(resources.getString(com.example.anothertimdatxe.R.string.update_driver_car_error_date_regis_empty))
         edt_date_regis.requestFocus()
     }
 
     override fun onDateRegisInFutureError() {
-        edt_date_regis.setError(resources.getString(R.string.update_driver_car_error_date_regis_future))
+        edt_date_regis.setError(resources.getString(com.example.anothertimdatxe.R.string.update_driver_car_error_date_regis_future))
         edt_date_regis.requestFocus()
     }
 
     override fun onDateRegistrationEmptyError() {
-        edt_handangkiem.setError(resources.getString(R.string.update_driver_car_error_registration_not_less_than_regiter_date))
+        edt_handangkiem.setError(resources.getString(com.example.anothertimdatxe.R.string.update_driver_car_error_registration_not_less_than_regiter_date))
         edt_handangkiem.requestFocus()
     }
 }
