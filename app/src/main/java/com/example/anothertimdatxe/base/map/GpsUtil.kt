24e6e898b9953a4cc934bbae58133da4a6b8fd07
HoangVietSE <viethoangtien@gmail.com>
2019-11-15 16:43:10 +0700

    Icon app timdatxe

--- app/src/main/java/com/example/anothertimdatxe/base/map/GpsUtil.kt
+++ app/src/main/java/com/example/anothertimdatxe/base/map/GpsUtil.kt
@@ -4,9 +4,10 @@ import android.content.Context
 import android.content.Context.LOCATION_SERVICE
 import android.content.IntentSender
 import android.location.LocationManager
+import android.util.Log
+import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.common.api.GoogleApiClient
-import com.google.android.gms.common.api.ResultCallback
-import com.google.android.gms.common.api.Status
+import com.google.android.gms.common.api.ResolvableApiException
 import com.google.android.gms.location.*
 
 
@@ -29,8 +30,8 @@ class GpsUtil(context: Context) {
 
         locationRequest = LocationRequest.create()
         locationRequest?.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
-        locationRequest?.interval = (10 * 1000).toLong()
-        locationRequest?.fastestInterval = (2 * 1000).toLong()
+        locationRequest?.interval = 10000
+        locationRequest?.fastestInterval = 10000 / 2
         val builder = LocationSettingsRequest.Builder()
                 .addLocationRequest(locationRequest!!)
         mLocationSettingsRequest = builder.build()
@@ -41,45 +42,44 @@ class GpsUtil(context: Context) {
     }
 
     // method for turn on GPS
-    fun turnGPSOn(OnGpsListener: OnGpsListener?, mGoogleApiClient: GoogleApiClient) {
-        if (locationManager!!.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-            OnGpsListener?.gpsStatus(true)
+    fun turnGPSOn(onGpsListener: OnGpsListener?, mGoogleApiClient: GoogleApiClient) {
+        if (locationManager!!.isProviderEnabled(LocationManager.GPS_PROVIDER)) onGpsListener?.let {
+            it.gpsStatus(true)
         } else {
-            val result = LocationServices.SettingsApi.checkLocationSettings(mGoogleApiClient, mLocationSettingsRequest)
-            result.setResultCallback(object : ResultCallback<LocationSettingsResult> {
-                override fun onResult(result: LocationSettingsResult) {
-                    val status = result.status
-                    val states = result.locationSettingsStates
-                    when (status.statusCode) {
-                        LocationSettingsStatusCodes.SUCCESS -> {
-                            OnGpsListener?.gpsStatus(true)
-                        }
-                        LocationSettingsStatusCodes.RESOLUTION_REQUIRED -> {
-                            // Location settings are not satisfied. But could be fixed by showing the user
-                            // a dialog.
-                            try {
-                                // Show the dialog by calling startResolutionForResult(),
-                                // and check the result in onActivityResult().
-                                OnGpsListener?.startResolution(status)
-                            } catch (e: IntentSender.SendIntentException) {
-                            }
-                        }
-                        LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE -> {
-                            // Location settings are not satisfied. However, we have no way to fix the
-                            // settings so we won't show the dialog.
-                            OnGpsListener?.startSettingGps()
+            mSettingsClient?.checkLocationSettings(mLocationSettingsRequest)
+                    ?.addOnSuccessListener {
+                        onGpsListener?.let {
+                            it.gpsStatus(true)
                         }
                     }
-                }
-
-            })
-
+                    ?.addOnFailureListener {
+                        when ((it as ApiException).statusCode) {
+                            LocationSettingsStatusCodes.RESOLUTION_REQUIRED -> {
+                                // Location settings are not satisfied. But could be fixed by showing the user
+                                // a dialog.
+                                try {
+                                    // Show the dialog by calling startResolutionForResult(), and check the
+                                    // result in onActivityResult().
+                                    onGpsListener?.startResolution(it as ResolvableApiException)
+                                } catch (sie: IntentSender.SendIntentException) {
+                                    Log.i(TAG, "PendingIntent unable to execute request.")
+                                }
+                            }
+                            LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE -> {
+                                // Location settings are not satisfied. However, we have no way to fix the
+                                // settings so we won't show the dialog.
+                                onGpsListener?.let {
+                                    it.startSettingGps()
+                                }
+                            }
+                        }
+                    }
         }
     }
 
     interface OnGpsListener {
         fun gpsStatus(isGPSEnable: Boolean)
-        fun startResolution(status: Status)
+        fun startResolution(rea: ResolvableApiException)
         fun startSettingGps()
     }
 
